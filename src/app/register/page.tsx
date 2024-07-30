39b47980855a18bdae75a2d34a291776525d6f57
tarou-jp <itohisahideki@gmail.com>
2024-07-31 02:39:16 +0900

    wip

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,16 +5,5 @@ import RegisterForm from "../../components/auth/RegisterForm";
 import { notFound } from "next/navigation";
 
 export default async function RegisterPage() {
-	const tokens = await getTokens(cookies(), {
-		apiKey: clientConfig.apiKey,
-		cookieName: serverConfig.cookieName,
-		cookieSignatureKeys: serverConfig.cookieSignatureKeys,
-		serviceAccount: serverConfig.serviceAccount,
-	});
-
-	if (!tokens) {
-		return notFound();
-	}
-
-	return <RegisterForm tokens={tokens} />;
+	return <RegisterForm />;
 }
