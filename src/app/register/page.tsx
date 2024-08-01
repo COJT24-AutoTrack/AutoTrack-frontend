bfaad04ca0fe13f3a4972dea27bd5c67e90eedce
iorin-io <iodere77@gmail.com>
2024-08-02 03:24:46 +0900

    registerからsignupに

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getTokens } from "next-firebase-auth-edge";
-import { cookies } from "next/headers";
-import { clientConfig, serverConfig } from "../../../config";
-import SignUpForm from "../../components/auth/SignUpForm";
-import { notFound } from "next/navigation";
-
-export default async function SignUpPage() {
-	return <SignUpForm />;
-}
